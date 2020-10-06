026f92a4d92bb58ef2a6315a7d7b91798cbd6786
Jasen Pan <jasenpan@gmail.com>
2020-10-06 22:04:48 +1100

    trigger deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hehehe</p>
+        <p>Try again</p>
         <a
           className="App-link"
           href="https://reactjs.org"
