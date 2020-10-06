ba587418256bfb775079205aa99db23b33e86d3b
Jasen Pan <jasenpan@gmail.com>
2020-10-06 22:11:22 +1100

    2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Try again</p>
+        <p>Try again 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
